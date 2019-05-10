c05cae8b4782ae2477fe5e09c35b1770ec41b5e3
lungen <lungenfetzer@gmail.com>
2019-05-10 21:38:21 +0200

    dev run

--- DataGrid/Article.cs
+++ DataGrid/Article.cs
@@ -36,5 +36,39 @@ namespace DataGrid
         {
             return $"{this.ArticleNumber}:{this.GS}:{this.ID}";
         }
+
+        public string ValidateArticle(string articleNumber)
+        {
+            if (articleNumber.Length < 3)
+                return "ArticleNumber to small";
+            else
+            {
+                this.ArticleNumber = articleNumber;
+                return "ok";
+            }
+        }
+
+        public string ValidateGS(string gs)
+        {
+            if(gs.Length != 2)
+                return "GS value too law."
+            else
+        {
+            GS = gs;
+                return "GS ok";
+            }
+        }
+
+        public string ValidateID(int id)
+        {
+            if (id < 0)
+                return "id has to be >= 0."
+            else
+            {
+                ID = id;
+                return "ID set ok";
+            }
+
+        }
     }
 }
